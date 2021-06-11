19c674acb92572aaaec3b530f2fc3834c54e5c51
David <david.dabela@outlook.com>
2021-06-11 09:48:10 -0500

    support more actions, feedback, and presets

--- HELP.md
+++ HELP.md
@@ -2,6 +2,12 @@
 ## PresentationTools APS
 Controls APS by [PresentationTools](https://presentationtools.com/).
 
+### Requirements
+* PC: APS v. 1.0.0.5 and above
+* Mac: APS v.2 (0) and above
+* Download latest version of APS from this website: [www.presentationtools.com/aps](https://presentationtools.com/aps)
+* _Note: Still-images requires APS v.2 and above_
+
 ### Configuration
 * IP address of the computer running APS for local use 127.0.0.1.
 * Port, the default is 4777.
@@ -15,3 +21,10 @@ Controls APS by [PresentationTools](https://presentationtools.com/).
 * Close all except current.
 #### Keystroke:
 * Send frequently used presentation keys (Right, Left, ESC and B)
+#### Capture Image:
+* Take screenshot of the main presenter-screen and store this in the image-banks.
+* Choose between 10 destinations.
+#### Display Image:
+* Display image on the main presenter screen. Choose between image-bank 1-10 or test-image, blackout or freeze screen. (Note: In APS preferences you can choose if this command is a toggle-button or not)
+#### Exit Images:
+* Exit APS-image from the main presenter screen
---
+++ choices.js
@@ -0,0 +1,33 @@
+exports.getChoicesForCapture = function () {
+    return [
+        { id: 'Capture1', label: 'Image 1' },
+        { id: 'Capture2', label: 'Image 2' },
+        { id: 'Capture3', label: 'Image 3' },
+        { id: 'Capture4', label: 'Image 4' },
+        { id: 'Capture5', label: 'Image 5' },
+        { id: 'Capture6', label: 'Image 6' },
+        { id: 'Capture7', label: 'Image 7' },
+        { id: 'Capture8', label: 'Image 8' },
+        { id: 'Capture9', label: 'Image 9' },
+        { id: 'Capture10', label: 'Image 10' }
+    ]
+}
+
+exports.getChoicesForDisplay = function () {
+    return [
+        { id: 'Display1', label: 'Image 1' },
+        { id: 'Display2', label: 'Image 2' },
+        { id: 'Display3', label: 'Image 3' },
+        { id: 'Display4', label: 'Image 4' },
+        { id: 'Display5', label: 'Image 5' },
+        { id: 'Display6', label: 'Image 6' },
+        { id: 'Display7', label: 'Image 7' },
+        { id: 'Display8', label: 'Image 8' },
+        { id: 'Display9', label: 'Image 9' },
+        { id: 'Display10', label: 'Image 10' },
+
+        { id: 'DisplayTest', label: 'Test' },
+        { id: 'Freeze', label: 'Freeze' },
+        { id: 'Blackout', label: 'Black' }
+    ]
+}
