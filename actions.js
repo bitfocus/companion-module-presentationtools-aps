1056fef476518a758941b5e5568507b5df2b7167
Mohammad Ahmad <eng.ma90@gmail.com>
2025-11-07 21:35:10 +0200

    Add Presentation Media Seek command for PowerPoint with direction and milliseconds options

--- actions.js
+++ actions.js
@@ -771,6 +771,33 @@ exports.getActions = function (instance) {
 			],
 			callback: action_callback,
 		},
+
+		Presentation_Media_Seek: {
+			name: 'Presentation: PowerPoint Media Seek',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Direction',
+					id: 'direction',
+					default: 'forward',
+					choices: [
+						{ id: 'forward', label: 'Forward'},
+						{ id: 'backward', label: 'Backward'},
+					],
+				},
+				{
+					type: 'number',
+					label: 'Milliseconds',
+					id: 'Milliseconds',
+					default: 1000,
+					min: 1,
+					step: 100,
+					required: true,
+					range: false,
+				},
+			],
+			callback: action_callback,
+		},
 	}
 }
 
@@ -1092,6 +1119,14 @@ exports.getCommandV2 = async function (action, instance) {
 			{
 				data.parameters = {
 					action: action.options.action,
+				}
+			}
+			break
+		case 'Presentation_Media_Seek':
+			{
+				data.parameters = {
+					direction: action.options.direction,
+					milliseconds: action.options.Milliseconds,
 				}
 			}
 			break
